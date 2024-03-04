b5c39494c4b9cb762c220d55c999395aac0723cb
sim1-dev <simonetenisci10@gmail.com>
2024-03-04 17:11:24 +0100

    [feat] IsGameDeveloperHandler: allow admin

--- Core/Middleware/Policies/IsGameDeveloperHandler.cs
+++ Core/Middleware/Policies/IsGameDeveloperHandler.cs
@@ -1,4 +1,8 @@
-// checks if the User has any Developer accounts which belongs to the Software House that created the specific Game
+/* fulfilled when User
+    - has admin role
+        OR
+    - has any Developer accounts which belongs to the Software House that created the specific Game
+*/
 
 using System.Security.Claims;
 using GamesAPI.Core.DataContexts;
@@ -26,6 +30,11 @@ public class IsGameDeveloperHandler : AuthorizationHandler<IsGameDeveloperRequir
 
         if(context.Resource is not HttpContext httpContext) {
             context.Fail();
+            return Task.CompletedTask;
+        }
+
+        if(httpContext.User.IsInRole("admin")){
+            context.Succeed(requirement);
             return Task.CompletedTask;
         }
 

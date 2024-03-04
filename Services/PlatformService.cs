64889f99fcf12339d9b28eb44694d680f2057475
sim1-dev <simonetenisci10@gmail.com>
2024-03-04 12:46:11 +0100

    [feat] PlatformService

---
+++ Services/PlatformService.cs
@@ -0,0 +1,59 @@
+using GamesAPI.Core.DataContexts;
+using GamesAPI.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class PlatformService {
+
+    private readonly BaseContext _db;
+
+    public PlatformService(BaseContext db) {
+       this._db = db;
+    }
+
+    public async Task<List<Platform>> GetAll() {
+        return await _db.Platforms.ToListAsync();
+    }
+    
+    public async Task<Platform?> Find(int id) {
+        Platform? platform = await _db.Platforms.FindAsync(id);
+
+        return platform;
+    }
+
+    public async Task<List<Platform>> FindByIds(List<int> ids) {
+        return await _db.Platforms.Where(platform => ids.Contains(platform.Id)).ToListAsync();
+    }
+
+    public async Task<Platform> Create(Platform platform) {
+        _db.Platforms.Add(platform);
+        await _db.SaveChangesAsync();
+
+        return platform;
+    }
+
+    public async Task<Platform?> Update(int id, Platform platform) {
+
+        Platform? existingPlatform = await this.Find(id);
+
+        if(existingPlatform is null)
+            return null;
+
+        this._db.Entry(existingPlatform).CurrentValues.SetValues(platform);
+
+        await this._db.SaveChangesAsync();
+
+        return existingPlatform;
+    }
+
+    public async Task<Platform?> Delete(int id) {
+        Platform? platform = await this.Find(id);
+
+        if(platform is null)
+            return null;
+
+        this._db.Platforms.Remove(platform);
+
+        return platform;
+    }
+
+}

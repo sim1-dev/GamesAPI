4c393bed6262d416281d510b018d55eca149512c
sim1-dev <simonetenisci10@gmail.com>
2024-03-05 17:35:15 +0100

    [fix] UpdateGameDto: legacy key

--- Core/Mapper/Dtos/Game/UpdateGameDto.cs
+++ Core/Mapper/Dtos/Game/UpdateGameDto.cs
@@ -6,7 +6,5 @@ public class UpdateGameDto {
     public DateTime ReleaseDate { get; set; }
     public string? ImageUrl {get; set;}
     public decimal Price {get; set;}
-    
-    public int PlatformId {get; set;}
     public int SoftwareHouseId { get; set; }
 }

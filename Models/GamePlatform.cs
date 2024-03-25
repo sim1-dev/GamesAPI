4a293b4f842c4cf7e69b2662127204ab537edd35
Simone Tenisci <simone.tenisci@r2u.it>
2024-03-25 12:00:30 +0100

    [refactor] Tables: gameplatform -> gameplatforms

--- Models/GamePlatform.cs
+++ Models/GamePlatform.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace GamesAPI.Models;
+[Table("gameplatforms")]
 public class GamePlatform {
     public int GameId { get; set; }
     public int PlatformId { get; set; }

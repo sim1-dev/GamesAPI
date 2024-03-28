fe27320aed76d1fcfadaa9a0038686438647aa5a
Simone Tenisci <simone.tenisci@r2u.it>
2024-03-28 12:56:18 +0100

    [feat] HTMLMailer: processTemplateParams - custom token example

--- Models/Mail/TestHTMLMailData.cs
+++ Models/Mail/TestHTMLMailData.cs
@@ -1,12 +1,37 @@
+using System.Text;
+using GamesAPI.Models;
+
 namespace GamesAPI.Core.Models;
 
-public class TestHTMLMailData : HTMLMailData
-{
+public class TestHTMLMailData : HTMLMailData {
     public TestHTMLMailData(MailHeader header, string subject = "Test subject HTML", string mailPath = MailTemplates.TEST) : base(header, subject, mailPath) {
 
     }
-
+    
+    // Simple example of a manual loop implementation. 
+    // It is not recommended to use this in a production environment, gotta find a proper way to use Razor templates
     public override string processTemplateParams(string templateBody) {
-        return string.Format(templateBody, "testParam1");
+        var categoriesPlaceholder = "{categories}";
+
+        var categories = new List<Category>() { 
+            new Category() { Name = "Test category 1" }, 
+            new Category() { Name = "Test category 2" } 
+        };
+
+        int index = templateBody.IndexOf(categoriesPlaceholder);
+
+        if(index == -1)
+            return templateBody;
+
+        StringBuilder categoriesBuilder = new StringBuilder();
+
+        foreach(Category category in categories) {
+            categoriesBuilder.AppendLine($"<li>{category.Name}</li>");
+        }
+
+        // Insert the list of categories into the template body
+        templateBody = templateBody.Remove(index, categoriesPlaceholder.Length).Insert(index, categoriesBuilder.ToString());
+
+        return templateBody;
     }
 }
